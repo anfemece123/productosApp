002c231e75e5207e2e29b9ef75c916b05789fdec
Andres Melo <andresfelipemelo18@gmail.com>
2023-07-27 21:10:41 -0500

    commit two

--- App.tsx
+++ App.tsx
@@ -4,12 +4,15 @@ import React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import { Navigator } from './src/navigator/Navigator';
 import { AuthProvider } from './src/context/AuthContext';
+import { ProductsProvider } from './src/context/ProductsContext';
 
 
 const AppState= ({children}:any)=> {
   return(
     <AuthProvider>
+      <ProductsProvider>
       {children}
+      </ProductsProvider>
     </AuthProvider>
   )
 }
